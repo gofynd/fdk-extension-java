fa835c57ae9390967ed1383f31e00e0326cfbdb8
azizwarsi <50980042+azizwarsi@users.noreply.github.com>
2021-07-13 15:34:39 +0530

    Create FdkConstants.java

---
+++ src/main/java/com/fynd/extension/constant/FdkConstants.java
@@ -0,0 +1,15 @@
+package com.fynd.extension.constant;
+
+import java.text.SimpleDateFormat;
+
+public class FdkConstants {
+
+    public static final String SESSION_COOKIE_NAME = "ext_session";
+
+    public static final ThreadLocal<SimpleDateFormat> DATE_FORMAT = new ThreadLocal<SimpleDateFormat>() {
+        @Override
+        protected SimpleDateFormat initialValue() {
+            return new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssXXX");
+        }
+    };
+}
