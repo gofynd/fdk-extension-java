3749f8b02a9d6c4c122615dbaa465a77d66df9f9
azizwarsi <azizwarsi@gofynd.com>
2021-11-16 17:24:09 +0530

    redis pool

--- src/main/java/com/fynd/extension/storage/RedisStorage.java
+++ src/main/java/com/fynd/extension/storage/RedisStorage.java
@@ -2,66 +2,74 @@ package com.fynd.extension.storage;
 
 
 import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
 
 import java.util.Map;
 
 public class RedisStorage extends BaseStorage{
 
-    private Jedis client;
+    private JedisPool pool;
     private String prefixKey;
 
-    public RedisStorage(Jedis client, String prefixKey) {
+    public RedisStorage(JedisPool pool, String prefixKey) {
         super(prefixKey);
-        this.client = client;
+        this.pool = pool;
     }
 
     @Override
     public String get(String key) {
 
-        String result = client.get(this.prefixKey + key);
-        client.close();
-        return result;
+        try(Jedis client = pool.getResource()) {
+            String result = client.get(this.prefixKey + key);
+            return result;
+        }
     }
 
     @Override
     public String set(String key,String value) {
-        String result = client.set(this.prefixKey + key,value);
-        client.close();
-        return result;
+        try(Jedis client = pool.getResource()) {
+            String result = client.set(this.prefixKey + key, value);
+            return result;
+        }
     }
 
     @Override
     public Long del(String key) {
-        Long result =client.del(this.prefixKey + key);
-        client.close();
-        return result;
+        try(Jedis client = pool.getResource()) {
+            Long result = client.del(this.prefixKey + key);
+            return result;
+        }
     }
 
     @Override
     public String setex(String key,int ttl,String value) {
-        String result = client.setex(this.prefixKey + key,ttl,value);
-        client.close();
-        return result;
+        try(Jedis client = pool.getResource()) {
+            String result = client.setex(this.prefixKey + key, ttl, value);
+            return result;
+        }
     }
 
     @Override
     public String hget(String key,String hashKey) {
-        String result =client.hget(this.prefixKey + key,hashKey);
-        client.close();
-        return result;
+        try(Jedis client = pool.getResource()) {
+            String result = client.hget(this.prefixKey + key, hashKey);
+            return result;
+        }
     }
 
     @Override
     public Long hset(String key, String hashKey, String value) {
-        Long result = client.hset(this.prefixKey + key,hashKey,value);
-        client.close();
-        return result;
+        try(Jedis client = pool.getResource()) {
+            Long result = client.hset(this.prefixKey + key, hashKey, value);
+            return result;
+        }
     }
 
     @Override
     public Map<String,Object> hgetall(String key) {
-        Map<String,Object> result =(Map)client.hgetAll(this.prefixKey + key);
-        client.close();
-        return result;
+        try(Jedis client = pool.getResource()) {
+            Map<String, Object> result = (Map) client.hgetAll(this.prefixKey + key);
+            return result;
+        }
     }
 }
